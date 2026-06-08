a20319ac1a6ee8dea85db6f1de6b0517a24be51b
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-08 18:38:06 +0300

    Fix __get__, __call__, __register, __setitem__
    -        return MethodType(instance, self)
    +        return MethodType(self, instance)
    
    -        stamp = tuple((parm.annotation for k, v sig.parameters.items() if k != "self"))
    +        stamp = tuple((v.annotation for v in sig.parameters.values()))[1:]
    
    Add stamp decorator

--- multi_method.py
+++ multi_method.py
@@ -2,38 +2,42 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 # flake8: noqa: F811
+from typing import Callable, Any, MutableMapping
 from types import MethodType
-from inspect import signature
+from functools import wraps
+from inspect import signature, _empty
 
 
 class MultiMethod:
     def __init__(self, name):
         self._name = name
         self.methods = {}
-        
 
     def __get__(self, instance, owner=None):
         if isinstance is None:
             return self
-        return MethodType(instance, self)
+        return MethodType(self, instance)
 
     def __call__(self, *args, **kwds):
-        stamp = (type(a) for a in args[1:])
+        stamp = tuple(type(a) for a in args)[1:]
         method = self.methods[stamp]
         return method(*args, **kwds)
-    
+
     def register(self, method):
         sig = signature(method)
-        stamp = tuple((parm.annotation for k, v sig.parameters.items() if k != "self"))
+        stamp = tuple((v.annotation for v in sig.parameters.values()))[1:]
         self.methods[stamp] = method
-        num_defaults = sum((v.default is not inspect._empty) for v in sig.parameters.values())
+        num_defaults = sum(v.default is not _empty for v in sig.parameters.values())
         if 0 < num_defaults:
-            self.methods[stamp[:-num_defaults]]
+            self.methods[stamp[:-num_defaults]] = method
 
 
 class MultiDict(dict):
     def __setitem__(self, key, val: Callable):
-        if (key not in self) or (key.startswith("__") and key.endswith("__")) :
+        if key.startswith("__") and key.endswith("__"):
+            super().__setitem__(key, val)
+            return
+        if key not in self:
             super().__setitem__(key, val)
             return
         oval: Callable = self[key]
@@ -48,7 +52,6 @@ class MultiDict(dict):
         super().__setitem__(key, mm)
 
 
-
 class MultiDispatch(type):
     @classmethod
     def __prepare__(
@@ -57,15 +60,27 @@ class MultiDispatch(type):
         return MultiDict()
 
 
-class Math(MultiDispatch):
+def stamp(func):
+    @wraps(func)
+    def wrapper(*args, **kwds):
+        sig = signature(func)
+        # print(list(v.annotation for k, v in sig.parameters.items() if k != "self"))
+        print(f"{func.__name__} {sig}")
+        res = func(*args, **kwds)
+        return res
+
+    return wrapper
+
+
+class Math(metaclass=MultiDispatch):
     def add(self, x: int, y: int):
-        pass
+        print(f"add-int-int({x}, {y})")
 
     def add(self, x: str, y: str):
-        pass
+        print(f"add-str-str({x}, {y})")
 
     def add(self, x: float, y: float = 0.0):
-        pass
+        print(f"add-float-float({x}, {y})")
 
 
 if __name__ == "__main__":
